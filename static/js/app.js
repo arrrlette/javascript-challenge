fd24f392a065db4891b262c89d5c4c1765a0b303
Arlette Varela <65184164+arrrlette@users.noreply.github.com>
2020-09-07 13:13:16 -0400

    upload

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+console.log(tableData[0]);
+
+//console.log(Object.keys(tableData));
+
+//for (var sightings in tableData){
+//    console.log(tableData[sightings])
+//};
+
+//for (var sightings in tableData){
+//    console.log(Object.entries(sightings))
+//};
--- static/js/index.js
+++
@@ -1,5 +0,0 @@
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-// Console.log the weather data from data.js
-console.log(data);
